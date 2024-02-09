5115b552254e972f5d1035c3e1b1bb0446d50f04
ArthurSapore <arthursapore17@gmail.com>
2024-02-09 11:03:28 -0300

    css module

--- react-app/src/components/Form/index.tsx
+++ react-app/src/components/Form/index.tsx
@@ -1,14 +1,19 @@
-import './style.scss'
+import style from './Form.module.scss'
 import Button from "../Button";
-
+/**
+ * CSS Modules, importante para não haver sobreposição de estilização.
+ * Altera o nome da classe adicionando um hash.
+ * Elimina a necessidade de se preocupar em criar classes css com o mesmo nome.
+ *
+ */
 export default function Form (){
     return(
-        <form action="" className='novaTarefa'>
-            <div className='inputContainer'>
+        <form action="" className={style.novaTarefa}>
+            <div className={style.inputContainer}>
                 <label htmlFor="tarefa">Adicione o que deseja estudar</label>
                 <input type="text" name="tarefa" id="tarefa" placeholder="O que você quer estudar" required />
             </div>
-            <div className='inputContainer'>
+            <div className={style.inputContainer}>
                 <label htmlFor="tempo">Tempo</label>
                 <input type="time" step="1" name="tempo" min="00:00:00" max="01:30:00" id="tempo" placeholder="O que você quer estudar" required />
             </div>
