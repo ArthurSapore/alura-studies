09d49e14bb82148d61da0d32505388b24d409a64
ArthurSapore <arthursapore17@gmail.com>
2024-02-13 20:07:59 -0300

    useEffect

--- react-app/src/components/watch/index.tsx
+++ react-app/src/components/watch/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ITarefa } from "../../types/tarefa";
 import Button from "../Button";
 import Time from "./Time";
@@ -13,10 +13,17 @@ type props = {
 export default function Watch({selecionado} : props){
 
     const [tempo, setTempo] = useState<Number>();
-    if(selecionado?.tempo){
-        setTempo(tempoParaSegundos(selecionado.tempo));
-    }
-
+    
+    /**
+     * useEffect utiliza dois parâmetros, o primeiro é a função que será executada e o segundo a variável que controlola-rá quando será executado
+     * Neste caso, a função será executada toda vez que o valor da variável 'selecionado' for alterado.
+     */
+    useEffect(()=>{
+        if(selecionado?.tempo){
+            setTempo(tempoParaSegundos(selecionado.tempo))
+        }
+    },[selecionado])
+    
     return(
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
