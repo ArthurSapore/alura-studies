fecf705b2e6b59c8a3c42a6b5b480466b3b81332
ArthurSapore <arthursapore17@gmail.com>
2024-02-18 21:35:45 -0300

    normalize css com styled-components

--- vite-project-space-app/src/App.tsx
+++ vite-project-space-app/src/App.tsx
@@ -1,4 +1,5 @@
 import { styled }from "styled-components"
+import GlobalStyles from "./components/Global"
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -10,7 +11,7 @@ function App() {
 
   return (
     <FundoGradiente>
-
+      <GlobalStyles />
     </FundoGradiente>
   )
 }
